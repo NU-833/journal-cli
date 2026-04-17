fdbfb71dce01e8cde2693eeb1f1a4e73991c7c18
NU-833 <NU833.newbee@proton.me>
2026-04-17 19:59:03 +0200

    Made simple model
    Model just save user text input and quit on enter

--- main.go
+++ main.go
@@ -6,3 +6,28 @@ import (
 
 	tea "charm.land/bubbletea/v2"
 )
+
+type model struct {
+	text	string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch {
+		case msg.Text != "":
+			m.text += msg.Text
+		case msg.Code == tea.KeyEnter:
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	return tea.NewView(m.text)
+}
