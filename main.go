208eddeee7746e3103dab864ffced59f8394e951
NU-833 <NU833.newbee@proton.me>
2026-04-23 23:55:04 +0200

    Write user input into file

--- main.go
+++ main.go
@@ -14,14 +14,14 @@ func main() {
 	now := time.Now()
 	fmt.Printf("%v %v %v %v %v:%v:%v\n", now.Weekday().String(), now.Day(), now.Month().String(), now.Year(), now.Hour(), now.Minute(), now.Second())
 	p := tea.NewProgram(uinp.Model{})
-	_, err := p.Run()
+	m, err := p.Run()
 	if err != nil {
 		fmt.Printf("There has been an error: %v", err)
 		os.Exit(1)
 	}
 
 	file_name := now.Format(time.RFC3339)
-	_, err = os.Create(file_name)
+	err = os.WriteFile(file_name, []byte(m.(uinp.Model).Text), 0644)
 	if err != nil {
 		fmt.Printf("There has been an error: %v", err)
 		os.Exit(1)
