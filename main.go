978ad64edd53beaf3c94935a0c840240bace1492
NU-833 <NU833.newbee@proton.me>
2026-04-24 23:45:36 +0200

    Declared a function for error checks

--- main.go
+++ main.go
@@ -10,20 +10,22 @@ import (
 	"journal-cli/uinp"
 )
 
+func check(err error) {
+	if err != nil {
+		fmt.Printf("There has been an error: %v", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	now := time.Now()
 	fmt.Printf("%v %v %v %v %v:%v:%v\n", now.Weekday().String(), now.Day(), now.Month().String(), now.Year(), now.Hour(), now.Minute(), now.Second())
 	p := tea.NewProgram(uinp.Model{})
 	m, err := p.Run()
-	if err != nil {
-		fmt.Printf("There has been an error: %v", err)
-		os.Exit(1)
-	}
+	check(err)
 
 	file_name := now.Format(time.RFC3339)
 	err = os.WriteFile(file_name, []byte(m.(uinp.Model).Text), 0644)
-	if err != nil {
-		fmt.Printf("There has been an error: %v", err)
-		os.Exit(1)
-	}
+	check(err)
+
 }
